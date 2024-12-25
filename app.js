f4f7f6b0adc2d1b55dfa1b612e1ea32f13965ba1
Gavin <gavinvm@protonmail.com>
2024-12-25 16:26:19 +0200

    feat: add passport to session management

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import sessionConfig from './config/session.js';
+import passport from './config/passport.js';
 
 const app = express();
 app.use(express.urlencoded({ extended: true })); // Parsing forms
 
 // Set up session
 sessionConfig(app);
+
+// Initialise passport and passport session
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(3000);
