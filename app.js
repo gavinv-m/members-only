d3540f1c69b671c0962ae9ee7eace6320f1c9bbc
Gavin <gavinvm@protonmail.com>
2024-12-25 16:32:37 +0200

    feat: Set template engine

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import sessionConfig from './config/session.js';
 import passport from './config/passport.js';
+import path from 'node:path';
 
 const app = express();
 app.use(express.urlencoded({ extended: true })); // Parsing forms
+app.set('views', path.join(process.cwd(), 'views'));
+app.set('view engine', 'ejs');
 
 // Set up session
 sessionConfig(app);
