e8a5ce0aaebcb151454d4168dba571acbc3693b5
Gavin <gavinvm@protonmail.com>
2024-12-26 12:45:52 +0200

    feat: Setup controller, and validate if empty

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,3 +1,28 @@
+import { body, validationResult } from 'express-validator';
+import passport from '../config/passport.js';
+
+const validateLogInRequest = [
+  body('username').trim().notEmpty().withMessage('Username is required'),
+  body('password').notEmpty().withMessage('Password is required'),
+];
+
+// Exports to login.js
+export const signInUser = [
+  validateLogInRequest,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty() === false) {
+      req.flash('errors', errors.array());
+      return res.redirect('/login');
+    }
+
+    passport.authenticate('local', {
+      failureRedirect: '/login',
+      successRedirect: '/',
+    });
+  },
+];
+
 // Exported to login.js
 export const renderLogin = (req, res) => {
   const errors = req.flash('errors');
--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { renderLogin } from '../controllers/login-controller.js';
+import { renderLogin, signInUser } from '../controllers/login-controller.js';
 
 const loginRoutes = Router();
 
 loginRoutes.get('/', renderLogin);
-loginRoutes.post('/');
+loginRoutes.post('/', signInUser);
 
 // Exports to index.js
 export default loginRoutes;
